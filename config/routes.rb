f3904012347d62148b2c4d3f17306e0dad018313
David Maurer <davemaurermail@gmail.com>
2015-11-30 15:34:35 -0700

    added routes for api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   root 'ideas#index'
+
+  resources :ideas, only: [:index, :edit, :update]
+
+  namespace api do
+    namespace v1 do
+      resources :ideas, only[:index, :edit, :update, :delete]
+    end
+  end
 end
