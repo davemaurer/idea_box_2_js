fdcb1e66cd88c16769cf2b0cbaf6b131552058e4
David Maurer <davemaurermail@gmail.com>
2015-11-30 22:16:53 -0700

    finally rendering ideas on the page. class vs id for div selector...

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -4,26 +4,22 @@ $(document).ready(function() {
 
 function getIdeas() {
   $.getJSON('/api/v1/ideas')
-    .then(function(response) {
-      $.each(response, function(i, idea) {
+    .then(function(ideas) {
+      ideas.forEach(function(idea) {
         renderIdea(idea)
       })
     })
 }
 
 function renderIdea(idea) {
-  appendIdea(idea);
-}
-
-function appendIdea(idea) {
   truncateIdea(idea);
   var newIdea = $('<div class="col-sm-10 col-sm-centered #ideas" data-id="'
-                  + idea.id
-                  + '"><h4>' + idea.title + '</h4>'
-                  + '"><h5>' + idea.body + '</h5>'
-                  + ' Quality: '
-                  + idea.quality
-                 );
+    + idea.id
+    + '"><h4>' + idea.title + '</h4>'
+    + '"><h5>' + idea.body + '</h5>'
+    + ' Quality: '
+    + idea.quality
+  );
   $('#ideas').append(newIdea)
 }
 
