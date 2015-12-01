a36a8263cd541ecbb87e281dfbe06c5ca869921d
David Maurer <davemaurermail@gmail.com>
2015-11-30 22:03:22 -0700

    add functions for idea render and new idea append

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -16,5 +16,19 @@ function renderIdea(idea) {
 }
 
 function appendIdea(idea) {
-  var newIdea = $('')
+  truncateIdea(idea);
+  var newIdea = $('<div class="col-sm-10 col-sm-centered #ideas" data-id="'
+                  + idea.id
+                  + '"><h4>' + idea.title + '</h4>'
+                  + '"><h5>' + idea.body + '</h5>'
+                  + ' Quality: '
+                  + idea.quality
+                 );
+  $('#ideas').append(newIdea)
+}
+
+function truncateIdea(idea) {
+  if(idea.body.length > 100) {
+    idea.body = idea.body.slice(0, 100) + '...';
+  }
 }
