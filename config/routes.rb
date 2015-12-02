df9b2dd460da1333210010440463dd719a6dd56d
David Maurer <davemaurermail@gmail.com>
2015-12-01 22:40:32 -0700

    fixed delete test by changing delete to destroy in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :ideas, only: [:index, :create, :update, :delete]
+      resources :ideas, only: [:index, :create, :update, :destroy]
     end
   end
 end
--- spec/features/user_creates_idea_spec.rb
+++ spec/features/user_creates_idea_spec.rb
@@ -5,16 +5,18 @@ RSpec.describe "User creates idea", type: :feature, js: true do
     it "can create a new idea" do
       visit root_path
 
-      fill_in "idea-title", with: "uneeke werds"
-      fill_in "idea-body", with: "so mi tst fynds yt"
-      click_on "Create Idea"
+      within("#create-idea") do
+        fill_in "idea-title", with:("uneeke werds")
+        fill_in "idea-body", with:("so mi tst fynds yt")
+        click_on "Create Idea"
+      end
 
       idea = Idea.find_by(title: "uneeke werds")
       expect(idea).not_to eq(nil)
 
       expect(current_path).to eq(root_path) #stay on page
-      expect(page).to have_content "uneeke werds"
-      expect(page).to have_content "fynds"
+      expect(page).to have_content("uneeke werds")
+      expect(page).to have_content("fynds")
     end
   end
 end
